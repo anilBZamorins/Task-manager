dc75fa8a65a95edf24f1c9a7ed8c34b3a11afdc7
anilBZamorins <anil.zamorins@gmail.com>
2025-02-09 10:31:56 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#12263F',
+        greylight:'#8A8A8A',
+        greyThinlight:'#F4F4F4',
+        grey:'#8A8894',
+        blackDark:'#181818',
+        blackDdark:'#202224',
+        danger:'#EA0234',
+        darklight:'#19334b',
+      },
+    },
   },
   plugins: [],
 }
